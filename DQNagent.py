608ddbfdff15c4914301b3a399d961f86d215a7e
Phil <philchen2000@gmail.com>
2018-11-18 19:48:22 -0800

    Modified model, added elements for MCTS compatibility

--- DQNagent.py
+++ DQNagent.py
@@ -1,38 +1,45 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential, Model
 from keras import layers
 from keras import optimizers
 import collections, random
-import tictactoe
+import tictactoe, util
 
 class DQNagent:
-	def __init__(self, state_size):
-		self.state_size = state_size
+	def __init__(self, dim=3):
+		self.state_size = (dim**2, dim**2, 4)
 		self.memory = collections.deque(maxlen=2000)
 		self.gamma = 0.95    # discount rate
 		self.epsilon = 1.0  # exploration rate
 		self.epsilon_min = 0.01
-		self.epsilon_decay = 0.995
-		self.learning_rate = 0.001
+		self.epsilon_decay = 0.9999
+		self.learning_rate = 0.0001
 		self.model = self._build_model()
 
 	def _build_model(self):
 		# Neural Net for Deep-Q learning Model
-		model = Sequential()
-		model.add(layers.Conv2D(24, (3,3), activation='relu', 
-					strides=(3,3), input_shape=self.state_size))
-		model.add(layers.Flatten())
-		# model.add(Dense(60, activation='relu'))
-		model.add(Dense(16, activation='relu'))
-		model.add(Dense(1, activation='tanh'))
-		model.compile(loss='binary_crossentropy',
-		              optimizer='rmsprop')
+		board_input = layers.Input(shape=self.state_size, name='board')
+		board_cnn = layers.Conv2D(24, (3,3), activation='relu', strides=(3,3)) (board_input)
+		dense_0 = layers.Flatten()(board_cnn)
+
+		dense_1 = layers.Dense(16, activation='relu')(dense_0)
+		probabilit = layers.Dense(1, activation='tanh')(dense_1)
+		model = Model(board_input, probability)
+		model.compile(loss='binary_crossentropy', optimizer='rmsprop')
 		model.summary()
 		return model
 
 	def remember(self, state, next_state, winner):
 		self.memory.append((state, next_state, winner))
+
+	def extractState(self, game):
+		state = np.zeros((1, self.state_size[0], self.state_size[1], self.state_size[2]))
+		state[0,:,:,0] = (game.getBoard() == 1)
+		state[0,:,:,1] = (game.getBoard() == -1)
+		state[0,:,:,2] = (game.getCurrPlayer() == 1)
+		for move in game.getMoves():
+			state[0,move[0],move[1],3] = 1
+		return state
 
 	def getAction(self, game):
 		moves = game.getMoves()
@@ -41,11 +48,27 @@ class DQNagent:
 			return moves[i]
 		q = []
 		for action in moves:
-			board = np.array(game.getBoard())
-			board[action] = game.getCurrPlayer()
-			board = board.reshape((1, self.state_size[0], self.state_size[1], self.state_size[2]))
-			q.append((self.model.predict(board) * game.getCurrPlayer(), action))
+			newState = game.generateSuccessor(action)
+			q.append((-self.getVal(newState), action))
+
 		return max(q)[1]
+
+	def getVal(self, game):
+		return self.model.predict(self.extractState(game))[0][0] * game.getCurrPlayer()
+
+	def getPolicy(self, game):
+		moves = game.getMoves()
+		vals = []
+		sumVals = 0
+		for action in moves:
+			newState = game.generateSuccessor(action)
+			val = 1 - self.getVal(newState)
+			vals.append(val)
+			sumVals += val
+		policy = collections.defaultdict(float)
+		for i, val in enumerate(vals):
+			policy[moves[i]] = val/sumVals
+		return policy
 
 	def replay(self, batch_size):
 		minibatch = random.sample(self.memory, batch_size)
